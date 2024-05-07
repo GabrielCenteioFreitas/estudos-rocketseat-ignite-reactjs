94ae2cee104c76329d02e51e362aeed2fc1dbf1a
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-06 23:40:50 -0300

    style: add font Inter

--- challenges/chapter03-ch01/src/pages/_document.tsx
+++ challenges/chapter03-ch01/src/pages/_document.tsx
@@ -1,7 +1,17 @@
-import Document from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+export default function Document() {
+  return (
+    <Html lang="pt-br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
