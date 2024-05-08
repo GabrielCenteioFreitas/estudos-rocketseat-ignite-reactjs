48cd0661e46a450fdab3a171cbe37123843a2c91
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-08 20:54:15 -0300

    feat: add prismic preview toolbar

--- challenges/chapter03-ch01/src/pages/_app.tsx
+++ challenges/chapter03-ch01/src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.scss';
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '../prismicio'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <PrismicPreview repositoryName={repositoryName} />
+    </>
+  );
 }
 
 export default MyApp;
