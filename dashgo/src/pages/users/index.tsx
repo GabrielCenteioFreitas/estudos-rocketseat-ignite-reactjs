3bc3e97023e183f04bf8b00c08e29827e1ff5853
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-19 16:09:09 -0300

    feat: add users list

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,27 +1,18 @@
 import { Header } from "@/components/Header";
 import { Pagination } from "@/components/Pagination";
 import { Sidebar } from "@/components/Sidebar";
+import { useUsers } from "@/services/hooks/useUsers";
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
-import { Suspense, useEffect } from "react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query'
+import { RiAddLine } from "react-icons/ri";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
-
-    return data
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
-
-  useEffect(() => {
-  }, [])
 
   return (
     <Box>
@@ -40,6 +31,10 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+
+              {!isLoading && isFetching && (
+                <Spinner size='sm' color='gray.500' ml='4' />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -77,50 +72,24 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]} color="gray.300" width="8">
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Gabriel Freitas</Text>
-                        <Text fontSize="sm" color="gray.300">gabriel@rocketseat.com.br</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && (
-                      <Td>04 de abril, 2021</Td>
-                    )}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={["4", "4", "6"]} color="gray.300" width="8">
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Gabriel Freitas</Text>
-                        <Text fontSize="sm" color="gray.300">gabriel@rocketseat.com.br</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && (
-                      <Td>04 de abril, 2021</Td>
-                    )}
-                  </Tr>
-                  
-                  <Tr>
-                    <Td px={["4", "4", "6"]} color="gray.300" width="8">
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Gabriel Freitas</Text>
-                        <Text fontSize="sm" color="gray.300">gabriel@rocketseat.com.br</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && (
-                      <Td>04 de abril, 2021</Td>
-                    )}
-                  </Tr>
+                  {data?.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]} color="gray.300" width="8">
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && (
+                          <Td>{user.createdAt}</Td>
+                        )}
+                      </Tr>
+                    )
+                  })}
                 </Tbody>
               </Table>
 
