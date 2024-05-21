764dbfbf03f6f2eec416353a900117c45d4e3ba0
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-20 23:24:24 -0300

    chore: initial configs

---
+++ Chapter-04/challenges/challenge-01/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+      <div>
+
+      </div>
+    </>
+  );
+}
