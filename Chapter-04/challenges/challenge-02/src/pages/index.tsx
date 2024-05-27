3148e0adfe3b4198148b492cb48b900c1a2c2bc5
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-27 17:44:56 -0300

    feat: add home page

--- Chapter-04/challenges/challenge-02/src/pages/index.tsx
+++ Chapter-04/challenges/challenge-02/src/pages/index.tsx
@@ -18,18 +18,34 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = null }) => {
+      const response = await api.get('/api/images', {
+        params: {
+          after: pageParam
+        }
+      })
+
+      return response.data
+    },
+    {
+      getNextPageParam:(response) => response.data.after
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const dataPages = data?.pages.map(page => page.data)
+    const dataPagesFlatted = dataPages?.flat()
+
+    return dataPagesFlatted
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />
+  }
 
   return (
     <>
@@ -37,7 +53,24 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        
+        {hasNextPage && (
+          <Button
+            py={10}
+            px="4"
+            bg="orange.500"
+            rounded="lg"
+            fontWeight="bold"
+            fontSize="md"
+            color="gray.50"
+            onClick={() => fetchNextPage()}
+          >
+            {!isFetchingNextPage
+              ? "Carregar mais"
+              : "Carregando..."
+            }
+          </Button>
+        )}
       </Box>
     </>
   );
