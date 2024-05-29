4a61f7b9a57cd4947cfc298d25497673f0ce06f2
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-28 21:01:33 -0300

    test: add Home page tests

---
+++ Chapter-05/ignews/src/tests/pages/Home.spec.tsx
@@ -0,0 +1,48 @@
+import Home, { getStaticProps } from '@/pages'
+import { stripe } from '../../services/stripe'
+import { render, screen } from '@testing-library/react'
+import { createMock } from 'ts-jest-mock'
+
+jest.mock('next-auth/react')
+jest.mock('next/router', () => {
+  return {
+    useSession: () => [null, false]
+  }
+})
+jest.mock('../../services/stripe')
+
+describe('Home page', () => {
+  it('renders correclty', () => {
+    render(
+      <Home product={ {
+          priceId: 'fake-price-id',
+          amount: 'R$10,00'
+        } }
+      />
+    )
+
+    expect(screen.getByText("for R$10,00 month")).toBeInTheDocument()
+  })
+
+  it('loads initial data', async () => {
+    const retrieveStripePricesMocked = createMock(stripe.prices.retrieve)
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000,
+    } as any)
+
+    const response = await getStaticProps({})
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00'
+          }
+        }
+      })
+    )
+  })
+})
