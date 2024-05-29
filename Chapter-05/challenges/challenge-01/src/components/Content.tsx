fa466d829d743dd432a9685b29d6e631aae065e0
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-05-29 14:18:57 -0300

    perf: add useMemo to movies list

--- Chapter-05/challenges/challenge-01/src/components/Content.tsx
+++ Chapter-05/challenges/challenge-01/src/components/Content.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { MovieCard } from "./MovieCard";
 
 interface ContentProps {
@@ -20,6 +21,18 @@ interface ContentProps {
 }
 
 export function Content({ selectedGenre, movies }: ContentProps) {
+  const moviesList = useMemo(() => {
+    return movies.map(movie => (
+      <MovieCard 
+        key={movie.imdbID}
+        title={movie.Title}
+        poster={movie.Poster}
+        runtime={movie.Runtime}
+        rating={movie.Ratings[0].Value}
+      />
+    ));
+  }, [movies]);
+  
   return (
     <div className="container">
       <header>
@@ -28,9 +41,7 @@ export function Content({ selectedGenre, movies }: ContentProps) {
 
       <main>
         <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
+          {moviesList}
         </div>
       </main>
     </div>
